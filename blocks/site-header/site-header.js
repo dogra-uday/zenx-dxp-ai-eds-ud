9ae8ab0951c9c695dd6bd49ecd0f9d1daaf10464
Dogra, Uday <uday.dogra+abcam@abcam.com>
2026-07-10 22:37:15 +0530

    siteheader

--- blocks/site-header/site-header.js
+++ blocks/site-header/site-header.js
@@ -1,7 +1,7 @@
 export default function decorate(block) {
-  const logoImage = 'https://upload.wikimedia.org/wikipedia/commons/5/53/Zensar_Logo.png';
+  const logoImage = '/content/dam/zenx-eds-site/zensar-logo.png';
 
-  const headerImage = 'https://images.unsplash.com/photo-1516321318423-f06f85e504b3?w=1600';
+  const headerImage = '/content/dam/zenx-eds-site/header-banner.jpg';
 
   const heading = 'Industry Verticals';
 
@@ -10,16 +10,16 @@ export default function decorate(block) {
   block.innerHTML = `
     <div class="site-header-wrapper">
       <div class="site-header-banner">
-        ${headerImage} alt="Industry Verticals Banner"
-          class="site-header-banner-image"
-          loading="eager"
-        >
+        ${headerImage}
       </div>
 
       <div class="site-header-content">
-        <img
-          src="${logoImage}"
-          alt="Zensar Logo"
-          class="site-header-div>
+        ${logoImage}
+
+        <h1>${heading}</h1>
+
+        <p>${subHeading}</p>
+      </div>
+    </div>
   `;
 }
